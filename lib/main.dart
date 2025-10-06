954623aa2d9e91b3b821463fc46708c7005ea44e
JosefineLundgren <guslunjofv@student.gu.se>
2025-10-06 10:22:54 +0200

    Formaterat koden och fixat bugg

--- lib/main.dart
+++ lib/main.dart
@@ -4,61 +4,83 @@ import './api.dart';
 import './ui.dart';
 import './model.dart';
 
-class MyState extends ChangeNotifier{
-  List<Todo> _todos = [               //list with the items included in the app's todolist
+class MyState extends ChangeNotifier {
+  List<Todo> _todos = [
+    //list with the items included in the app's todolist
     /*Todo(title: 'Write a book'),
       Todo(title: 'Tidy room', done: true),*/
   ];
 
-  MyState(){
-    fetchTodos();  //uses getTodo()
+  MyState() {
+    fetchTodos(); //uses getTodo()
   }
 
   String _filter = 'all';
 
-  List<Todo> get todos{
-    if (_filter == 'done'){                               //returns a new list with only the todos where todo.done==true
+  List<Todo> get todos {
+    if (_filter == 'done') {
+      //returns a new list with only the todos where todo.done==true
       return _todos.where((todo) => todo.done).toList();
-    }
-    else if (_filter == 'undone'){
-      return _todos.where((todo) => !todo.done).toList(); //returns a new list with only the todos where todo.done==false
+    } else if (_filter == 'undone') {
+      return _todos
+          .where((todo) => !todo.done)
+          .toList(); //returns a new list with only the todos where todo.done==false
     }
     return _todos; //for "all"
   }
 
-   Future<void> removeTodo(Todo todo) async{
-    await deleteTodo(todo.id);               //delete the id corresponding to the todo
-    _todos.remove(todo);                     //delete todo from the list
+  Future<void> removeTodo(Todo todo) async {
+    await deleteTodo(todo.id); //delete the id corresponding to the todo
+    _todos.remove(todo); //delete todo from the list
     notifyListeners();
-   }
-  
-    Future<void> checkTodo(Todo todo, bool? value) async{  
-    todo.done = value ?? false;               //if value is null, set todo.done to false
-    notifyListeners();
-    await putTodo(todo.id, todo.done, todo.title);  //all variables are needed to change the "state" of checkbox (to not get for ex. an empty title)
   }
-  void setFilter(String filter){
+
+  Future<void> checkTodo(Todo todo, bool? value) async {
+    var oldValue = todo.done;
+    var newValue = value ?? false;
+
+    todo.done = newValue; //if value is null, set todo.done to false
+    notifyListeners();
+
+    try {
+      await putTodo(todo.id, newValue, todo.title);
+      //all variables are needed to change the "state" of checkbox (to not get for ex. an empty title)
+    } catch (e) {
+      todo.done =
+          oldValue; //if the server call with putTodo was not successful, "un-do" the check (local change)
+      notifyListeners();
+    }
+  }
+
+  void setFilter(String filter) {
     _filter = filter;
     notifyListeners();
   }
-  Future<void> fetchTodos() async{           //uses getTodos() which returns a list of todos from the API to use here
+
+  Future<void> fetchTodos() async {
+    //uses getTodos() which returns a list of todos from the API to use here
     var todos = await getTodos();
     _todos = todos;
     notifyListeners();
   }
-  Future<void> listNewTodo(String title) async{   //addTodo() got replaced to this
-    await postTodo(title);               //uses the API to add a todo, and fetch the new updated list of todos
-    await fetchTodos();                
+
+  Future<void> listNewTodo(String title) async {
+    //addTodo() got replaced to this
+    await postTodo(
+      title,
+    ); //uses the API to add a todo, and fetch the new updated list of todos
+    await fetchTodos();
   }
 }
 
 void main() {
-
   runApp(
-    ChangeNotifierProvider(create: (context) => MyState(),  //creates a new object of class MyState(), the constructor then runs automatically
-    child: MyApp(),
+    ChangeNotifierProvider(
+      create: (context) =>
+          MyState(), //creates a new object of class MyState(), the constructor then runs automatically
+      child: MyApp(),
     ),
-    );
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -68,8 +90,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TODO assignment',
-     home: MyHomePage(title: 'assignment',)
+      home: MyHomePage(title: 'assignment'),
     );
   }
 }
-
--- lib/model.dart
+++ lib/model.dart
@@ -1,16 +1,21 @@
-class Todo {         //"model-class" with title, done (true/false) and id
+class Todo {
+  //"model-class" with title, done (true/false) and id
   String title;
   bool done;
   String id;
 
-  Todo({required this.title, this.done = false, required this.id}); //id added for "putTodo"
+  Todo({
+    required this.title,
+    this.done = false,
+    required this.id,
+  }); //id added for "putTodo"
 
-  factory Todo.fromJson(Map<String, dynamic> json) {      //the JSON format is "converted" to a Todo instance
-  return Todo(
-    title: json['title'].toString(),     
-    done: json['done'] ?? false,
-    id: json['id'].toString(),
-  );
-}
-
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    //the JSON format is "converted" to a Todo instance
+    return Todo(
+      title: json['title'].toString(),
+      done: json['done'] ?? false,
+      id: json['id'].toString(),
+    );
+  }
 }
